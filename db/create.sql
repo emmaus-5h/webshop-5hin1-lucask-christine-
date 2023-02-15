1253233a625b0eb7304d0aa8d76dd166052a4981
Christine2811 <74548101+Christine2811@users.noreply.github.com>
2023-02-15 10:00:36 +0000

    tabel erbij

--- db/create.sql
+++ db/create.sql
@@ -25,15 +25,14 @@ CREATE TABLE reviews(
   product_id INTEGER
  );
 
-CREATE TABLE orders( 
- id INTEGER PRIMARY KEY AUTOINCREMENT,
-  orders INTEGER ,
-  product_id INTEGER
- );
-
-CREATE TABLE console(
+CREATE TABLE consoles(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  platform TEXT
+  console TEXT
+);
+
+CREATE TABLE genres(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  genre TEXT
 );
 --
 -- populate with data
@@ -44,16 +43,16 @@ CREATE TABLE console(
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Minecraft', 'blokgame', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Mario', 'loodgieter meneer spel', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Valorant', 'First Person Shooter', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Fortnite', '100 player battle royale', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Counter Strike: Global offensive', 'First Person Shooter', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Overwatch', 'First Person Shooter', '492662523-7', 14);
-insert into products (name, description, code, price) values ('Call of Duty', 'First Person Shooter', '472663823-6', 12);
-insert into products (name, description, code, price) values ('Mortal Kombat', 'Fight game', '138962523-0', 15.5);
-insert into products (name, description, code, price) values ('Smash bros', 'Fight game', '293952523-2', 11);
-insert into products (name, description, code, price) values ('GTA', 'First Person Shooter', '592880514-4', 13);
+insert into products (name, description, code, price, console_id) values ('Minecraft', 'blokgame', '816905633-0', 10.5, 4);
+insert into products (name, description, code, price, console_id) values ('Mario', 'loodgieter meneer spel', '077030122-3', 11, 3);
+insert into products (name, description, code, price, console_id) values ('Valorant', 'First Person Shooter', '445924201-X', 13.5, 4);
+insert into products (name, description, code, price, console_id) values ('Fortnite', '100 player battle royale', '693155505-7', 13.5, 1);
+insert into products (name, description, code, price, console_id) values ('Counter Strike: Global offensive', 'First Person Shooter', '686928463-6', 14, 4);
+insert into products (name, description, code, price, console_id) values ('Overwatch', 'First Person Shooter', '492662523-7', 14, 4);
+insert into products (name, description, code, price, console_id) values ('Call of Duty', 'First Person Shooter', '472663823-6', 12, 2);
+insert into products (name, description, code, price, console_id) values ('Mortal Kombat', 'Fight game', '138962523-0', 15.5, 2);
+insert into products (name, description, code, price, console_id) values ('Smash bros', 'Fight game', '293952523-2', 11, 2);
+insert into products (name, description, code, price, console_id) values ('GTA', 'First Person Shooter', '592880514-4', 13, 1);
 
 insert into persons (name) values ('Lucas')
 insert into persons (name) values ('Christine')
@@ -67,18 +66,17 @@ insert into reviews (review, person_id, product_id) values ("3/5", 2, 1)
 insert into reviews (review, person_id, product_id) values ("5/5", 2, 10)  
 insert into reviews (review, person_id, product_id) values ("4/5", 3, 4)
 insert into reviews (review, person_id, product_id) values ("3/5", 4, 7) 
-insert into reviews (review, person_id, product_id) values ("2/5", 4, 8)
-  
+insert into reviews (review, person_id, product_id) values ("2/5", 4, 8)  
 insert into reviews (review, person_id, product_id) values ("5/5", 5, 3)
 insert into reviews (review, person_id, product_id) values ("1/5", 5, 9)
+  
+insert into consoles (console) values ("xbox")
+insert into consoles (console) values ("playstation")
+insert into consoles (console) values ("nintendo")
+insert into consoles (console) values ("pc")
 
-insert into console (platform) values ("xbox")
-insert into console (platform) values ("playstation")
-insert into console (platform) values ("playstation")
-insert into console (platform) values ("xbox")
-insert into console (platform) values ("nintendo")
-insert into console (platform) values ("xbox")
-insert into console (platform) values ("playstation")
-insert into console (platform) values ("playstation")
-insert into console (platform) values ("xbox")
-insert into console (platform) values ("nintendo")
+insert into genres (genre) values ("first-person shooter")
+insert into genres (genre) values ("fight game")
+insert into genres (genre) values ("Action-adventure")
+insert into genres (genre) values ("platform")
+
