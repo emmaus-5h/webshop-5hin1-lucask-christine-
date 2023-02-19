9e1c5f0e341d57bb0b3a823fa492a30018195c3c
Christine2811 <74548101+Christine2811@users.noreply.github.com>
2023-02-19 00:33:18 +0000

    bux gefixt

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.get('/api/products/:id', getProductById)
 app.get('/api/reviews', getReviews)
 app.get('/api/consoles', getConsoles)
 app.get('/api/genres', getGenres)
-app.get('/api/persons', getPersons)
+//app.get('/api/persons', getPersons)
 
 //app.get('/api/products/:id/related', db.getRelatedProductsById)
 // our API is not protected...so let's not expose these
@@ -72,7 +72,7 @@ function getCategories(request, response) {
 function getProducts(request, response) {
   console.log('API ontvangt /api/products/', request.query)
   let data = []
-  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.code AS code, products.price AS price FROM products ORDER BY name JOIN consoles ON console_id = products.console_id')
+  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.code AS code, products.price AS price FROM products ORDER BY name')
   data = sqlOpdracht.all()
   // console.log(JSON.stringify(data, null, 2))
   response.status(200).send(data)
